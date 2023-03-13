4a9cd6da8fc40fb93ef908883f0e0d8200ff44b4
kidgvbs <kidgvbs@proton.me>
2023-03-13 14:51:45 +0100

    draw function implemented - probabilty calculator

--- probability-calculator.py
+++ probability-calculator.py
@@ -5,11 +5,29 @@ import random
 class Hat:
     def __init__(self, **balls): # **balls -> significa che la funzione accetta un numero arbitrario di argomenti chiave-valore passati come un dizionario
         if len(balls) < 1:
-            raise ValueError("Deve esserci minimo un argomento") 
+            raise ValueError("Deve esserci minimo un argomento")
             # raise -> solleva un'eccezione
             # ValueError -> è un tipo di eccezione che viene sollevata quando si passa un valore non valido a una funzione o metodo
-        else: self.contents = balls
-    
+        else: 
+            self.contents = list()
+            # inserisci il colore della pallina nella lista tante volte quante è il suo valore
+            for color,count in balls.items():
+                for i in range(count):
+                    self.contents.append(color)
+        
+    def __repr__(self):
+        return str(self.contents)
+
+    def draw(self, n_balls):
+        if n_balls > len(self.contents):
+            raise ValueError("more balls to draw than balls in the hat")
+        else:
+            balls_drawn = []
+            for i in range(n_balls):
+                ball_index = random.randint(0, len(self.contents)-1)
+                balls_drawn.append(self.contents.pop(ball_index))
+                
+        
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
@@ -17,4 +35,4 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
 
 # TEST
 hat1 = Hat()
-print(hat1) # output = {'yellow': 2, 'red': 5, 'blue': 1}
+print(hat1)
